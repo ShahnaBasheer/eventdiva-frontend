ed11c158317745d8cabc73bd7f1b89ab940dc03a
SHAHNABASHEER <shahana.basheer.in@gmail.com>
2024-09-19 21:43:28 +0530

    sixth  commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,31 @@
+
+
+
+export const environment = {
+  production: true,
+  google_client_id: '1064683432489-6jae1gj7ej8ljr5jd04pi2ucgihuivgb.apps.googleusercontent.com',
+  baseUrl: 'https://backend.eventdiva.online:2000',
+  vendorUrl: 'https://backend.eventdiva.online/vendor',
+  adminUrl: 'https://backend.eventdiva.online/admin',
+  ep_portfolio_url: '/uploads/Event%20Planners/Portfolios/',
+  ep_coverpic_url: '/uploads/Event%20Planners/CoverPic/',
+  ep_doc_url: '/uploads/Event%20Planners/Documents/',
+  vv_portfolio_url: '/uploads/Venues/Portfolios/',
+  vv_coverpic_url: '/uploads/Venues/CoverPic/',
+  vv_doc_url: '/uploads/Venues/Documents/',
+  ad_accessKey: 'ad_access',
+  vn_accessKey: 'vn_access',
+  cu_accessKey: 'cu_access',
+  ad_refreshKey: 'ad_refresh',
+  cu_refreshKey: 'cu_refresh',
+  vn_refreshKey: 'vn_refresh',
+  vendorInfo: 'vendorInfo',
+  adminInfo: 'adminInfo',
+  customerInfo: 'customerInfo',
+  customer: 'customer',
+  vendor: 'vendor',
+  admin: 'admin',
+  venue_vendor: 'venue-vendor',
+  event_planner: 'event-planner',
+  razor_key: 'rzp_test_FmNCCXUWBBloc6'
+};
