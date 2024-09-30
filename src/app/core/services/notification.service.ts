7c3e351dceca65bb0f3c870f863a083060239fcc
SHAHNABASHEER <shahana.basheer.in@gmail.com>
2024-09-30 18:41:22 +0530

    addes changes

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Socket } from 'socket.io-client';
@@ -7,14 +6,12 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { SocketService } from './socket.service';
 import { Store } from '@ngrx/store';
 import { getUserId as isCustomer } from '../../features/customer/store/customer.selectors';
-import { getUserId as isVendor} from '../../features/vendors/store/vendor.selectors';
+import { getUserId as isVendor } from '../../features/vendors/store/vendor.selectors';
 import { Notification } from '../models/notification.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class NotificationService {
   private socket!: Socket | null;
   private notificationsSubject = new BehaviorSubject<Notification[]>([]);
@@ -27,21 +24,19 @@ export class NotificationService {
     private socketService: SocketService,
     private store: Store
   ) {
-
-    this.store.select(isCustomer).subscribe(id => {
+    this.store.select(isCustomer).subscribe((id) => {
       if (id) {
         this.initializeSocketConnection(environment.customer, id);
         this.userId = id;
       }
     });
 
-    this.store.select(isVendor).subscribe(id => {
+    this.store.select(isVendor).subscribe((id) => {
       if (id) {
         this.initializeSocketConnection(environment.vendor, id);
         this.userId = id;
       }
     });
-
   }
 
   private initializeSocketConnection(role: string, id: string) {
@@ -52,11 +47,17 @@ export class NotificationService {
   }
 
   private initializeSocketListeners(role: string, id: string) {
-    this.socket?.on('loaded-notification', (data: { notification: Notification;  }) => {
-      const currentNotifications = this.notificationsSubject.getValue();
-      console.log(data.notification, "NOtification Arrived Here")
-      this.notificationsSubject.next([data.notification, ...currentNotifications]);
-    });
+    this.socket?.on(
+      'loaded-notification',
+      (data: { notification: Notification }) => {
+        const currentNotifications = this.notificationsSubject.getValue();
+        console.log(data.notification, 'NOtification Arrived Here');
+        this.notificationsSubject.next([
+          data.notification,
+          ...currentNotifications,
+        ]);
+      }
+    );
 
     // this.socket?.on('video-call-invitation', (data: { callerId: string, roomId: string }) => {
     //   const videoCallNotification = {
@@ -69,41 +70,51 @@ export class NotificationService {
     // });
   }
 
-
-
   public fetchNotifications(url: string = ''): Observable<any> {
     return this.http.get(`${url}/notifications`, {
-        withCredentials: true
+      withCredentials: true,
     });
   }
 
   // Emit notification event to server
-  emitNotification(data: { message: string, userId: string, role: string, type: string }): void {
+  emitNotification(data: {
+    message: string;
+    userId: string;
+    role: string;
+    type: string;
+  }): void {
     this.socketService.emit('save-notifications', {
-        message:  data.message, userId: data.userId, role: data.role , type: data.type
+      message: data.message,
+      userId: data.userId,
+      role: data.role,
+      type: data.type,
     });
   }
 
-  public getUserId(){
+  public getUserId() {
+  console.log("kndckn")
     return this.userId;
   }
 
-  public addNotifications(data: Notification[]){
+  public addNotifications(data: Notification[]) {
     return this.notificationsSubject.next(data);
   }
 
-  public onIsReadChange(id: string, url: string = ''){
-      return this.http.patch(`${url}/notifications/read`, { id  }, {
-        withCredentials: true
-      });
+  public onIsReadChange(id: string, url: string = '') {
+    return this.http.patch(
+      `${url}/notifications/read`,
+      { id },
+      {
+        withCredentials: true,
+      }
+    );
   }
 
-  public ondeleteNotification(id: string, url: string = ''){
+  public ondeleteNotification(id: string, url: string = '') {
     return this.http.delete(`${url}/notifications/delete/${id}`, {
-      withCredentials: true
+      withCredentials: true,
     });
   }
-
 
   public emitNewNotification(event: string, data: any) {
     this.socket?.emit('save-notification', data);
